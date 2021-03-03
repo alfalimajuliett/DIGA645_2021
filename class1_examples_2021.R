7f0b7811a02c9b4ba65517fefda8e3ecae4f95ec
alfalimajuliett <patt0335@umn.edu>
2021-03-02 20:23:15 -0600

    class 1 demos with more comments and fixed the factors and levels demos

--- class1_examples_2021.R
+++ class1_examples_2021.R
@@ -1,18 +1,22 @@
 b <- read.csv("~/src/generations/bnl.csv")
 # dont forget to install babynames package first 
-library(babynames)
+library(babynames) #calling in a package that contains the dataset babynames
+
+(?(babynames)) #info on the dataset
+str(babynames) #structure of the dataset
+head(babynames)
 
 bryn <- babynames[(babynames$name == "Bryn")&(babynames$sex == "M"),] # all rows that match that selection, all columns
-plot(bryn$n ~ bryn$year)
-(Bryn <- subset(babynames, babynames$name == "Bryn"))
-Bryn_sub <- subset(Bryn, Bryn$sex == "M")
+plot(bryn$n ~ bryn$year) #plot takes a formula y data by x data
+(Bryn <- subset(babynames, babynames$name == "Bryn")) #subset out this name
+Bryn_sub <- subset(Bryn, Bryn$sex == "M") #subset of subset for males
 
 plot(Bryn_sub$n ~ Bryn_sub$year, ylab = "n", xlab = "year", main = "babies named bryn")
 
-sub_year <- babynames[babynames$year==1991,]
+sub_year <- babynames[babynames$year==1991,] #all columns for rows where year is 1991 
 sub_year[order(-sub_year$n),]
 
-my_vec <- c("q", 5, TRUE)
+my_vec <- c("q", 5, TRUE) #R will coerce all the elements to string type
 
 (raster1 <- matrix( data = c(1,0,0,1), nrow = 4, ncol = 2))
 (raster2 <- matrix(data = c(0, 1, 1, 1), nrow = 4, ncol = 2))
@@ -22,6 +26,8 @@ city <- c("Chicago", "Des Moines", "Duluth", "Fargo")
 state <- c("IL", "IA", "MN", "ND")
 latitude <- c(41.5,41.35,46.49,46.52)
 longitude <- c(87.37,93.37,92.5,96.48)
+
+#data.frame "glues" the vectors together, each their own column
 cities_df <- data.frame(city,state,latitude,longitude)
 
 names(cities_df) <- c("City_name", "State", "Lat", "Lon")
@@ -40,9 +46,10 @@ my_list <- list(nums, logicals, strings)
 
 landcover_type <- c("forest", "edge", "forest", "water", "swamp", "water")
 
-landcover_type <- factor(landcover_type)
-landcover_type <- levels(landcover_type)[1:3] <- "land"
-landcover_type <- levels(landcover_type)[4] <- "water"
+landcover_type <- factor(landcover_type) #sorts covers into categories
+
+#sorting into two levels, land and water. I don't usually have to work with levels, but it could happen
+levels(landcover_type) <- c("land","water") #oh geez, I was getting too fancy with my subseting, this was all I needed to do
 
 
 
